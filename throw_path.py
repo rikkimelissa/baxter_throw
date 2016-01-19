38b2090fa04127aa9716fc908ecdd6ca84af81ff
Rikki <rikkiirwin@gmail.com>
2016-01-18 20:50:24 -0600

    working on trajectory

--- throw_path.py
+++ throw_path.py
@@ -2,6 +2,7 @@
 """
 Baxter RSDK Joint Trajectory Action Client Example
 rosrun baxter_interface joint_trajectory_action_server.py
+rosrun baxter_examples joint_recorder.py -f "test"
 """
 import argparse
 import sys
@@ -26,6 +27,8 @@ from baxter_interface import CHECK_VERSION
 
 import numpy as np
 
+import rosbag
+
 class Trajectory(object):
     def __init__(self, limb):
         ns = 'robot/limb/' + limb + '/'
@@ -34,7 +37,7 @@ class Trajectory(object):
             FollowJointTrajectoryAction,
         )
         self._goal = FollowJointTrajectoryGoal()
-        self._goal_time_tolerance = rospy.Time(0.1)
+        self._goal_time_tolerance = rospy.Time(0.2)
         self._goal.goal_time_tolerance = self._goal_time_tolerance
         server_up = self._client.wait_for_server(timeout=rospy.Duration(10.0))
         if not server_up:
@@ -52,16 +55,15 @@ class Trajectory(object):
         point.accelerations = copy(accelerations)
         point.time_from_start = rospy.Duration(time)
         self._goal.trajectory.points.append(point)
-        print('point added')
 
     def add_point_p(self, positions,time):
         point = JointTrajectoryPoint()
         point.positions = copy(positions)
         point.time_from_start = rospy.Duration(time)
         self._goal.trajectory.points.append(point)
-        print('point added')
 
     def start(self):
+        print('started')
         self._goal.trajectory.header.stamp = rospy.Time.now()
         self._client.send_goal(self._goal)
 
@@ -133,7 +135,8 @@ def main():
     q_throw = np.array([ 0.55703127,  1.02672179,  2.36138413,  1.08580171,  3.05891801,
      -1.42679103, -0.42898074])
     q_end = np.array([ 0.84862266 ,  0.97046182 ,  2.15649338 ,  1.77949586,   2.03984311 ,  1.74583931, 
-    N = 10
+      1.06574319])
+    N = 1
 
     path1 = JointTrajectorySpeedUp(q_start,q_throw,N,100,1);
     path2 = JointTrajectorySlowDown(q_throw,q_start,N,100,1)
@@ -147,22 +150,28 @@ def main():
     acc2 = np.vstack(([0,0,0,0,0,0,0],acc2))
     tSpace = np.linspace(0,N,100)
 
-    traj.add_point_p(path1[0,:],7)
+    traj.add_point_p(path1[0,:],3)
+    traj.start()
+    traj.wait(7)
 
     for i in range(100):
         pos = path1[i,:]
         vel = vel1[i,:]
         acc = acc1[i,:]
         time = tSpace[i]
-        traj.add_point(pos,vel,acc,time)
-    for i in range(100):
-        pos = path2[i,:]
-        vel = vel2[i,:]
-        acc = acc2[i,:]
-        time = tSpace[i] + N
-        traj.add_point(pos,vel,acc,time)
+        # traj.add_point(pos,vel,acc,time)
+        traj.add_point_p(pos,time)
+
+    # for i in range(100):
+    #     pos = path2[i,:]
+    #     vel = vel2[i,:]
+    #     acc = acc2[i,:]
+    #     time = tSpace[i] + N
+    #     traj.add_point(pos,vel,acc,7 + N + time)
 
     traj.start()
+    print(traj.result())
+
     traj.wait(15.0)
     print("Exiting - Joint Trajectory Action Test Complete")
 
