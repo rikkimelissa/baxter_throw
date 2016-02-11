04368a7594c9022831656d4d5470e6093ce4cbed
Rikki <rikkiirwin@gmail.com>
2016-02-11 15:34:10 -0600

    fixed bugs and plotted results for release state sampling

--- src/release_state_sample.py
+++ src/release_state_sample.py
@@ -3,14 +3,15 @@
 from math import sqrt, cos, sin, tan, atan, pi, atan2
 from random import random
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Define range and static variables
 x_range = [.6,.8] # near x position of catcher
 y_range = [-.62,.24] # between start position and catch position
 z_range = [.1,.4] # between catch position and high position of hand
 vel_range_x = [0,.1] # need to experiment with these ranges
-vel_range_y = [.3, 1.2] # needs minimum velocity for liftoff but can't be so high that JTAS will fail
-vel_range_z = [0, .6]
+vel_range_y = [.3, 1.5] # needs minimum velocity for liftoff but can't be so high that JTAS will fail
+vel_range_z = [0, 1.5]
 cup_height = .12
 block_height = .018
 block_width = .047
@@ -18,19 +19,10 @@ g = 9.8 # m/s^2
 
 # These are set by user
 catch_x = .68
-catch_z = 0
-catch_y = .29 
-
-throw_x = .65
-throw_y = -.08
-throw_z = .313
-
-dx = catch_x - throw_x
-dy = catch_y - throw_y - block_width
-dz = catch_z + cup_height - throw_z - block_height
+catch_z = -.6 # range from - .5 to 0 # lower range determined by baxter
+catch_y = .7 # range from .29 to .5, works after .5 but hard to find solutions
 
 def plot_results():
-    throw_x_list = [None]*100
     throw_y_list = [None]*100
     throw_z_list = [None]*100
     vel_list = [None]*100
@@ -39,29 +31,32 @@ def plot_results():
         res = test_pos(catch_x, catch_y, catch_z)
         while res == None:
             res = test_pos(catch_x, catch_y, catch_z)
-        throw_x = res[0]; throw_y = res[1]; throw_z = res[2]; vel = res[3]; alpha = res[4]
-        throw_x_list[i] = throw_x;
+        throw_y = res[0]; throw_z = res[1]; vel = res[2]; alpha = res[3]
         throw_y_list[i] = throw_y;
         throw_z_list[i] = throw_z;
         vel_list[i] = vel;
         alpha_list[i] = alpha;
     N = 100
     ind = range(N)
-    width = 1
-    fig, ax = plt.subplots()
+    # width = 1
+    # fig, ax = plt.subplots()
     # rect2 = ax.bar([x + y for x, y in zip(ind, [width]*100)],throw_y_list, width, color = 'r')
-    rect2 = ax.bar([x for x in ind],throw_y_list, width, color = 'y')
-    rect3 = ax.bar([x+100 for x in ind],throw_z_list, width, color = 'g')
-    rect4 = ax.bar([x+200 for x in ind],vel_list, width, color = 'b')
-    rect5 = ax.bar([x+300 for x in ind],alpha_list, width, color = 'c')
+    # rect2 = ax.bar([x for x in ind],throw_y_list, width, color = 'y')
+    # rect3 = ax.bar([x+100 for x in ind],throw_z_list, width, color = 'g')
+    # rect4 = ax.bar([x+200 for x in ind],vel_list, width, color = 'b')
+    # rect5 = ax.bar([x+300 for x in ind],alpha_list, width, color = 'c')
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(throw_y_list, throw_z_list, vel_list)
+    plt.show(block=False)
+    ax.set_xlabel('Y position (m)')
+    ax.set_ylabel('Z position (m)')
+    ax.set_zlabel('Velocity (m/s^2)')
+    title = "Throw position vs throw velocity for catch position of " + str(catch_y) + "," + str(catch_z)
+    ax.set_title(title)
     plt.show(block=False)
 
-    # print "x = " + str(throw_x) + "  y = " + str(throw_y) + "  z = " + str(throw_z)
-    # print "vel = " + str(vel) + "     alpha = " + str(alpha)
-
 def test_pos(catch_x, catch_y, catch_z):
-    # rand = random()
-    # print "rand = " + str(rand)
     throw_x = random()*(x_range[1] - x_range[0]) + x_range[0]
     throw_y = random()*(y_range[1] - y_range[0]) + y_range[0]
     throw_z = random()*(z_range[1] - z_range[0]) + z_range[0]
@@ -76,7 +71,7 @@ def test_pos(catch_x, catch_y, catch_z):
 def find_velocity_param(dx,dy,dz, rand):
     alpha_min = atan2(dz,dy)
     if alpha_min <= (-pi/2 + .01):
-        alpha_min = pi/2 + .01
+        alpha_min = -pi/2 + .01
     alpha_max = 75*pi/180
     inc = 0
     alpha_list = []
@@ -84,6 +79,7 @@ def find_velocity_param(dx,dy,dz, rand):
     for i in range(1000):
         alpha = rand*(alpha_max - alpha_min) + alpha_min
         if ((g*dy**2/(2*cos(alpha)**2*(dy*tan(alpha)-dz))) < 0):
+            x = 1;
             print rand, alpha, dy, dz
         v = sqrt(g*dy**2/(2*cos(alpha)**2*(dy*tan(alpha)-dz)))
         v_y = v*cos(alpha)
@@ -92,7 +88,6 @@ def find_velocity_param(dx,dy,dz, rand):
         p_y = v_y*t
         p_z = t*v_z - .5*g*t**2
         if (v_y >= vel_range_y[0] and v_y <= vel_range_y[1] and v_z >= vel_range_z[0] and v_z <= vel_range_z[1]):
-            # print inc, v_y, v_z, p_y, p_z  
             alpha_list.append(alpha)
             v_list.append(v)
             inc += 1 
