7617e7c749ef6b89358d8836cd67a32c026da7fb
Rikki <rikkiirwin@gmail.com>
2016-02-15 22:57:15 -0600

    started building path

--- src/rrt.py
+++ src/rrt.py
@@ -6,27 +6,80 @@ from math import atan2, sin, cos
 import time
 from release_state_solve import find_feasible_release
 
-pos_init = [2,2,0,0]
-pos_goal = [95,95,15,20]
-iter = 500;
 
-treeA = np.empty((iter+1,4))
-edgesA = np.empty((iter+1,2))
-treeB = np.empty((iter+1,4))
-edgesB = np.empty((iter+1,2))
-treeA[0] = pos_init
-treeB[0] = pos_goal
 
-def dynamic_rrt():
-    # catch_x = .68
-    # catch_z = -.6 # range from - .5 to 0 # lower range determined by baxter
-    # catch_y = .7 # range from .29 to .5, works after .5 but hard to find solutions
-    # throw_y, throw_z, vel, alpha = find_feasible_release(catch_x, catch_y, catch_z)
-    # example end point
-    throw_y = .20516
-    throw_z = .3073
-    vel = 1.03
-    alpha = 1.55
+# def dynamic_rrt():
+#     # catch_x = .68
+#     # catch_z = -.6 # range from - .5 to 0 # lower range determined by baxter
+#     # catch_y = .7 # range from .29 to .5, works after .5 but hard to find solutions
+#     # throw_y, throw_z, vel, alpha = find_feasible_release(catch_x, catch_y, catch_z)
+#     # example end point
+#     throw_y = .20516
+#     throw_z = .3073
+#     vel = 1.03
+#     alpha = 1.55
+
+def find_path():
+    pos_init = [2,2,0,0]
+    pos_goal = [95,95,15,20]
+    iter = 500;
+
+    treeA = np.empty((iter+1,4))
+    edgesA = np.empty((iter+1,2))
+    treeB = np.empty((iter+1,4))
+    edgesB = np.empty((iter+1,2))
+    treeA[0] = pos_init
+    treeB[0] = pos_goal
+    treeA, treeB, edgesA, edgesB = build_tree(iter,treeA, treeB ,edgesA, edgesB) 
+
+    plt.figure()
+    plt.plot(treeB)
+    plt.show(block=False)
+
+    pathA = np.empty((edgesA.shape[0],4))
+    length = edgesA.shape[0]
+    pathA[0] = treeA[length]
+    curEdge = edgesA[length-1]
+    ind = 1
+    atOrigin = False
+    while atOrigin == False:
+        pathA[ind] = treeA[curEdge[0]]
+        curEdge = edgesA[curEdge[0] - 1]
+        ind += 1
+        if curEdge[0] == 0:
+            atOrigin = True
+    pathA = pathA[0:ind,:]
+
+    plt.figure()
+    plt.plot(pathA[:,0],pathA[:,1])
+    plt.show(block=False)
+
+    pathB = np.empty((edgesB.shape[0],4))
+    length = edgesB.shape[0]
+    pathB[0] = treeB[length]
+    curEdge = edgesB[length-1]
+    ind = 1
+    atOrigin = False
+    while atOrigin == False:
+        pathB[ind] = treeB[curEdge[0]]
+        curEdge = edgesB[curEdge[0] - 1]
+        ind += 1
+        if curEdge[0] == 0:
+            atOrigin = True
+    pathB = pathB[0:ind,:]
+
+    plt.figure()
+    plt.plot(pathB[:,0],pathB[:,1])
+    plt.show(block=False)
+
+
+
+
+
+
+
+
+
 
 def build_tree(iter,treeA, treeB ,edgesA, edgesB):
     i = 0
@@ -71,7 +124,7 @@ def build_tree(iter,treeA, treeB ,edgesA, edgesB):
         plt.plot([treeA[indA,0], treeB[indB,0]],[treeA[indA,1], treeB[indB,1]],'g',linewidth = 3)
     plt.xlim([0,100])
     plt.ylim([0,100])
-    for k in range(i):
+    for k in range(i+1):
         plt.plot([treeA[edgesA[k,0]][0],treeA[edgesA[k,1]][0]],[treeA[edgesA[k,0]][1],treeA[edgesA[k,1]][1]],'r')
         plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
     plt.show(block=False)
@@ -90,6 +143,7 @@ def build_tree(iter,treeA, treeB ,edgesA, edgesB):
     #     plt.plot([treeA[edgesA[k,0]][2],treeA[edgesA[k,1]][2]],[treeA[edgesA[k,0]][3],treeA[edgesA[k,1]][3]],'r')
     #     # plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
     # plt.show(block=False)
+    return treeA[0:i+2,:], treeB[0:i+2,:], edgesA[0:i+1,:], edgesB[0:i+1,:]
 
 def connect_trees(treeA, treeB, iter):
     for i in range(iter+1):
@@ -148,7 +202,8 @@ def nearest_neighbor(x, y, vx, vy, tree, nodes):
     return min_node
 
 if __name__ == "__main__":
-    start = time.time()
-    build_tree(iter,treeA, treeB ,edgesA, edgesB)
-    end = time.time()
-    print end-start
+    x = 1
+    # start = time.time()
+    # build_tree(iter,treeA, treeB ,edgesA, edgesB)
+    # end = time.time()
+    # print end-start
