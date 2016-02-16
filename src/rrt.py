9c444a5429a19a34960c04bebe458edc841c1a56
Rikki <rikkiirwin@gmail.com>
2016-02-15 22:06:13 -0600

    control law mostly working

--- src/rrt.py
+++ src/rrt.py
@@ -6,13 +6,13 @@ from math import atan2, sin, cos
 import time
 from release_state_solve import find_feasible_release
 
-pos_init = [2,2]
-pos_goal = [95,95]
-iter = 1000;
+pos_init = [2,2,0,0]
+pos_goal = [95,95,15,20]
+iter = 500;
 
-treeA = np.empty((iter+1,2))
+treeA = np.empty((iter+1,4))
 edgesA = np.empty((iter+1,2))
-treeB = np.empty((iter+1,2))
+treeB = np.empty((iter+1,4))
 edgesB = np.empty((iter+1,2))
 treeA[0] = pos_init
 treeB[0] = pos_goal
@@ -30,14 +30,14 @@ def dynamic_rrt():
 
 def build_tree(iter,treeA, treeB ,edgesA, edgesB):
     i = 0
-    vA = np.array([0,0])
-    vB = np.array([0,0])
     while i < iter:
         x = random()*100
         y = random()*100
-        node = nearest_neighbor(x,y, treeA, i)
+        vx = random()*50 - 25
+        vy = random()*50 - 25
+        node = nearest_neighbor(x,y,vx,vy,treeA, i)
         # choose_control
-        treeA, vA = insert_vertex(node,treeA,x,y,i, vA, 1)
+        treeA = insert_vertex(node,treeA,x,y,vx,vy,i,1)
         edgesA = insert_edge(edgesA, node, i)
         # x = random()*100
         # y = random()*100
@@ -62,8 +62,8 @@ def build_tree(iter,treeA, treeB ,edgesA, edgesB):
     plt.figure()
     plt.plot(treeA[0,0],treeA[0,1],'ro',markersize = 10)
     plt.plot(treeA[0:i+2,0],treeA[0:i+2,1],'r.')
-    plt.plot(treeB[0,0],treeB[0,1],'ro',markersize = 10)
-    plt.plot(treeB[0:i+2,0],treeB[0:i+2,1],'b.')
+    # plt.plot(treeB[0,0],treeB[0,1],'ro',markersize = 10)
+    # plt.plot(treeB[0:i+2,0],treeB[0:i+2,1],'b.')
     # if (result[0]):
     #     indA = result[1];
     #     indB = result[2];
@@ -71,8 +71,22 @@ def build_tree(iter,treeA, treeB ,edgesA, edgesB):
     plt.xlim([0,100])
     plt.ylim([0,100])
     for k in range(i-1):
-        print k
         plt.plot([treeA[edgesA[k,0]][0],treeA[edgesA[k,1]][0]],[treeA[edgesA[k,0]][1],treeA[edgesA[k,1]][1]],'r')
+        # plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
+    plt.show(block=False)
+    plt.figure()
+    plt.plot(treeA[0,2],treeA[0,3],'ro',markersize = 10)
+    plt.plot(treeA[0:i+2,2],treeA[0:i+2,3],'r.')
+    # plt.plot(treeB[0,0],treeB[0,1],'ro',markersize = 10)
+    # plt.plot(treeB[0:i+2,0],treeB[0:i+2,1],'b.')
+    # if (result[0]):
+    #     indA = result[1];
+    #     indB = result[2];
+    #     plt.plot([treeA[indA,0], treeB[indB,0]],[treeA[indA,1], treeB[indB,1]],'g',linewidth = 3)
+    plt.xlim([-25, 25])
+    plt.ylim([-25, 25])
+    for k in range(i):
+        plt.plot([treeA[edgesA[k,0]][2],treeA[edgesA[k,1]][2]],[treeA[edgesA[k,0]][3],treeA[edgesA[k,1]][3]],'r')
         # plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
     plt.show(block=False)
 
@@ -83,18 +97,18 @@ def connect_trees(treeA, treeB, iter):
                 return True, i, k
     return False, 0, 0
 
-def insert_vertex(node,tree,x,y,i,v,dir):
+def insert_vertex(node,tree,x,y,vx,vy,i,dir):
     forceControls = np.array(([1,0],[-1,0],[0,1],[0,-1]))
-    mass = .1 #???
-    dt = .01 # seconds
+    mass = 10 #???
+    dt = .1 # seconds
     error = 1000000
     for force in forceControls:
         a = force/mass;
-        v_new = v + a*dt
+        v_new = [vx,vy] + a*dt
         if dir > 0:
-            p = tree[node] + v_new*dt
+            p = tree[node,0:2] + v_new*dt
         else:
-            p = tree[node] - v_new*dt
+            p = tree[node,0:2] - v_new*dt
         # print a, v_new, p
         error_new = (p[0] - x)**2 + (p[1] - y)**2
         if error_new < error:
@@ -107,8 +121,8 @@ def insert_vertex(node,tree,x,y,i,v,dir):
     # yc = 2*sin(alpha) + tree[node,1]
     # print xc, yc, vc
 
-    tree[i+1] = [xc,yc]
-    return tree, vc
+    tree[i+1] = [xc,yc,vc[0],vc[1]]
+    return tree
 
 def insert_edge(edges, node, i):
     edges[i] = [node, i+1]
@@ -117,11 +131,15 @@ def insert_edge(edges, node, i):
 # def choose_control(node, x, y):
 #     angle = 
 
-def nearest_neighbor(x, y, tree, nodes):
+def nearest_neighbor(x, y, vx, vy, tree, nodes):
     min_dist = 100000000
     min_node = 0
+    wp = .5
+    wv = 1 - wp
     for i in range(nodes+1):
-        dist = (x - tree[i,0])**2 + (y-tree[i,1])**2
+        distPos = (x - tree[i,0])**2 + (y-tree[i,1])**2
+        distVel = (vx - tree[i,2])**2 + (vy-tree[i,3])**2
+        dist = wp*distPos + wv*distVel;
         # print i, dist
         if (dist < min_dist):
             min_dist = dist
