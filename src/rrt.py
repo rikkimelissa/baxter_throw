161b910068ac78a700975e2ac92a258ecdd69b68
Rikki <rikkiirwin@gmail.com>
2016-02-17 21:45:23 -0600

    updated rrt with realistic path parameters

--- src/rrt.py
+++ src/rrt.py
@@ -6,31 +6,23 @@ from math import atan2, sin, cos
 import time
 from release_state_solve import find_feasible_release
 
+def find_path(plot):
 
-
-# def dynamic_rrt():
-#     # catch_x = .68
-#     # catch_z = -.6 # range from - .5 to 0 # lower range determined by baxter
-#     # catch_y = .7 # range from .29 to .5, works after .5 but hard to find solutions
-#     # throw_y, throw_z, vel, alpha = find_feasible_release(catch_x, catch_y, catch_z)
-#     # example end point
-#     throw_y = .20516
-#     throw_z = .3073
-#     vel = 1.03
-#     alpha = 1.55
-
-def find_path():
-    pos_init = [2,2,0,0]
-    pos_goal = [95,95,15,20]
-    iter = 500;
+    pos_init = [-.62, -.1, 0, 0]
+    throw_y = .20516
+    throw_z = .3073
+    vel = 1.03
+    alpha = 1.55
+    pos_goal = [throw_y, throw_z, vel*cos(alpha), vel*sin(alpha)]
+    iter = 1000;
 
     treeA = np.empty((iter+1,4))
-    edgesA = np.empty((iter+1,2))
+    edgesA = np.empty((iter,2))
     treeB = np.empty((iter+1,4))
-    edgesB = np.empty((iter+1,2))
+    edgesB = np.empty((iter,2))
     treeA[0] = pos_init
     treeB[0] = pos_goal
-    treeA, treeB, edgesA, edgesB, indA, indB = build_tree(iter,treeA, treeB ,edgesA, edgesB) 
+    treeA, treeB, edgesA, edgesB, indA, indB = build_tree(iter,treeA, treeB ,edgesA, edgesB, plot) 
 
     pathA = np.empty((edgesA.shape[0],4))
     pathA[0] = treeA[indA]
@@ -44,7 +36,9 @@ def find_path():
         if curEdge[0] == 0:
             atOrigin = True
     pathA = pathA[0:ind,:]
-    plt.plot(pathA[:,0],pathA[:,1],'g',linewidth=2)
+
+    if (plot):
+        plt.plot(pathA[:,0],pathA[:,1],'g',linewidth=2)
 
     pathB = np.empty((edgesB.shape[0],4))
     pathB[0] = treeB[indB]
@@ -59,27 +53,35 @@ def find_path():
             atOrigin = True
     pathB = pathB[0:ind,:]
 
-    plt.plot(pathB[:,0],pathB[:,1],'g',linewidth=2)
-    plt.show(block=False)
+    if (plot):
+        plt.plot(pathB[:,0],pathB[:,1],'g',linewidth=2)
+        plt.show(block=False)
 
     path = np.vstack((pathA[::-1],pathB))
+
+    if (plot):
+        plt.figure()
+        plt.plot(path,'.')
+        plt.show(block=False)
+
     return path
 
-def build_tree(iter,treeA, treeB ,edgesA, edgesB):
+def build_tree(iter,treeA, treeB ,edgesA, edgesB, plot):
     i = 0
     while i < iter:
-        x = random()*100
-        y = random()*100
-        vx = random()*40 - 15
-        vy = random()*40 - 15
+
+        x = random()*.2 + .6
+        y = random()*.86 -.62
+        vx = random()*1.2 + .3
+        vy = random()*1.5
         node = nearest_neighbor(x,y,vx,vy,treeA, i)
         treeA = insert_vertex(node,treeA,x,y,vx,vy,i,1)
         edgesA = insert_edge(edgesA, node, i)
 
-        x = random()*100
-        y = random()*100
-        vx = random()*40 - 15
-        vy = random()*40 - 15
+        x = random()*.2 + .6
+        y = random()*.86 -.62
+        vx = random()*1.2 + .3
+        vy = random()*1.5
         node = nearest_neighbor(x,y,vx,vy,treeB, i)
         treeB = insert_vertex(node,treeB,x,y,vx,vy,i,-1)
         edgesB = insert_edge(edgesB, node, i)
@@ -87,62 +89,50 @@ def build_tree(iter,treeA, treeB ,edgesA, edgesB):
         if (result[0]):
             print i
             break
-        # print x, y, node, treeA[0:i+2, :], edgesA[0:i+1,:]
-        # plt.plot(treeA[0:i+2,0],treeA[0:i+2,1],'.')
-        # for k in range(i+1):
-        #     plt.plot([treeA[edgesA[k,0]][0],treeA[edgesA[k,1]][0]],[treeA[edgesA[k,0]][1],treeA[edgesA[k,1]][1]])
-        # plt.xlim([1.5, 3])
-        # plt.ylim([1.5, 3])
-        # plt.show(block=False)
         i = i + 1
 
-    plt.close('all')
-    plt.figure()
-    plt.plot(treeA[0,0],treeA[0,1],'ro',markersize = 10)
-    plt.plot(treeA[0:i+2,0],treeA[0:i+2,1],'r.')
-    plt.plot(treeB[0,0],treeB[0,1],'ro',markersize = 10)
-    plt.plot(treeB[0:i+2,0],treeB[0:i+2,1],'b.')
+    if (plot):
+        plt.close('all')
+        plt.figure()
+        plt.plot(treeA[0,0],treeA[0,1],'ro',markersize = 10)
+        plt.plot(treeA[0:i+2,0],treeA[0:i+2,1],'r.')
+        plt.plot(treeB[0,0],treeB[0,1],'ro',markersize = 10)
+        plt.plot(treeB[0:i+2,0],treeB[0:i+2,1],'b.')
     if (result[0]):
         indA = result[1];
         indB = result[2];
-        plt.plot([treeA[indA,0], treeB[indB,0]],[treeA[indA,1], treeB[indB,1]],'g',linewidth = 3)
-    plt.xlim([0,100])
-    plt.ylim([0,100])
-    for k in range(i+1):
-        plt.plot([treeA[edgesA[k,0]][0],treeA[edgesA[k,1]][0]],[treeA[edgesA[k,0]][1],treeA[edgesA[k,1]][1]],'r')
-        plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
-    plt.show(block=False)
-    # plt.figure()
-    # plt.plot(treeA[0,2],treeA[0,3],'ro',markersize = 10)
-    # plt.plot(treeA[0:i+2,2],treeA[0:i+2,3],'r.')
-    # # plt.plot(treeB[0,0],treeB[0,1],'ro',markersize = 10)
-    # # plt.plot(treeB[0:i+2,0],treeB[0:i+2,1],'b.')
-    # # if (result[0]):
-    # #     indA = result[1];
-    # #     indB = result[2];
-    # #     plt.plot([treeA[indA,0], treeB[indB,0]],[treeA[indA,1], treeB[indB,1]],'g',linewidth = 3)
-    # plt.xlim([-25, 25])
-    # plt.ylim([-25, 25])
-    # for k in range(i):
-    #     plt.plot([treeA[edgesA[k,0]][2],treeA[edgesA[k,1]][2]],[treeA[edgesA[k,0]][3],treeA[edgesA[k,1]][3]],'r')
-    #     # plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
-    # plt.show(block=False)
+        if (plot):
+            plt.plot([treeA[indA,0], treeB[indB,0]],[treeA[indA,1], treeB[indB,1]],'g',linewidth = 2)
+    else:
+        indA = 0
+        indB = 0
+    if (plot):
+        plt.xlim([-.7, .5])
+        plt.ylim([-.5, .7])
+        for k in range(i):
+            plt.plot([treeA[edgesA[k,0]][0],treeA[edgesA[k,1]][0]],[treeA[edgesA[k,0]][1],treeA[edgesA[k,1]][1]],'r')
+            plt.plot([treeB[edgesB[k,0]][0],treeB[edgesB[k,1]][0]],[treeB[edgesB[k,0]][1],treeB[edgesB[k,1]][1]],'b')
+        plt.show(block=False)
+
     return treeA[0:i+2,:], treeB[0:i+2,:], edgesA[0:i+1,:], edgesB[0:i+1,:], indA, indB
 
 def connect_trees(treeA, treeB, iter):
     for i in range(iter+1):
-        if ((treeA[i,0] - treeB[iter+1,0])**2 + (treeA[i,1] - treeB[iter+1,1])**2) < .5:
+        if ((treeA[i,0] - treeB[iter+1,0])**2 + (treeA[i,1] - treeB[iter+1,1])**2) < .001:
             return True, i, iter+1
     for k in range(iter+1):
-        if ((treeA[iter+1,0] - treeB[k,0])**2 + (treeA[iter+1,1] - treeB[k,1])**2) < .5:
+        if ((treeA[iter+1,0] - treeB[k,0])**2 + (treeA[iter+1,1] - treeB[k,1])**2) < .001:
             return True, iter+1, k
     return False, 0, 0
 
 def insert_vertex(node,tree,x,y,vx,vy,i,dir):
+
     forceControls = np.array(([1,0],[-1,0],[0,1],[0,-1]))
-    mass = 10 #???
-    dt = .1 # seconds
+    mass = 1 #???
+    dt = .01 # seconds
     error = 1000000
+
+    # choose the control that pulls the tree toward the random point
     for force in forceControls:
         a = force/mass;
         v_new = [vx,vy] + a*dt
@@ -150,7 +140,6 @@ def insert_vertex(node,tree,x,y,vx,vy,i,dir):
             p = tree[node,0:2] + v_new*dt
         else:
             p = tree[node,0:2] - v_new*dt
-        # print a, v_new, p
         error_new = (p[0] - x)**2 + (p[1] - y)**2
         if error_new < error:
             error = error_new
@@ -164,9 +153,6 @@ def insert_vertex(node,tree,x,y,vx,vy,i,dir):
 def insert_edge(edges, node, i):
     edges[i] = [node, i+1]
     return edges
-
-# def choose_control(node, x, y):
-#     angle = 
 
 def nearest_neighbor(x, y, vx, vy, tree, nodes):
     min_dist = 100000000
@@ -184,8 +170,7 @@ def nearest_neighbor(x, y, vx, vy, tree, nodes):
     return min_node
 
 if __name__ == "__main__":
-    x = 1
-    # start = time.time()
-    # build_tree(iter,treeA, treeB ,edgesA, edgesB)
-    # end = time.time()
-    # print end-start
+    start = time.time()
+    find_path(True)
+    end = time.time()
+    print end-start
