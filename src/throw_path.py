04087544e30e12f98c63ce3b957f408b480ba559
Rikki <rikkiirwin@gmail.com>
2016-02-28 16:38:30 -0600

    working path execution, modified launch file

--- src/throw_path.py
+++ src/throw_path.py
@@ -55,6 +55,7 @@ from functions import JointTrajectory
 import numpy as np
 from solve_linear_system import linearSpace
 from std_msgs.msg import Float32MultiArray
+from rospy.numpy_msg import numpy_msg
 
 class Trajectory(object):
     def __init__(self, limb):
@@ -74,6 +75,9 @@ class Trajectory(object):
             rospy.signal_shutdown("Timed out waiting for Action Server")
             sys.exit(1)
         self._sub = rospy.Subscriber('joint_path', numpy_msg(Float32MultiArray), self.path_cb)
+        self._limb_interface = baxter_interface.limb.Limb('right')
+        self._q_start = np.array([-0.22281071, -0.36470393,  0.36163597,  1.71920897, -0.82719914,
+       -1.16889336, -0.90888362])
         self.clear(limb)
 
     def add_point_p(self, positions, time):
@@ -120,117 +124,36 @@ class Trajectory(object):
         rospy.loginfo('Received path')
         pList = a.data;
         pMat = np.reshape(pList,(pList.shape[0]/14,14))
-        pArm = pMat[:,0:7]
-        vArm = pMat[:,7:14]
-        N = pArm.shape[0]
-        T = 2
+        self._pArm = pMat[:,0:7]
+        self._vArm = pMat[:,7:14]
+        self.execute()
+
+    def execute(self):
+        N = self._pArm.shape[0]
+        T = 10
         t_all = np.linspace(0 + self._t_delay,T + self._t_delay, N);
+
+        current_angles = [self._limb_interface.joint_angle(joint) for joint in self._limb_interface.joint_names()]
+        self.add_point_p(current_angles, 0.0)
+        self.add_point_p(self._q_start,self._t_delay)
+
         for i in range(N):
-            self.add_point_pv(pArm[i,:].tolist(), vArm[i,:].tolist(),t_all[i])
+            self.add_point_pv(self._pArm[i,:].tolist(), self._vArm[i,:].tolist(),t_all[i])
         # traj.add_point_p(thList[i,:].tolist(), t_all[i])
         self.start()
         self.wait(10)
+        self.clear('right')
 
 
 def main():
-    """RSDK Joint Trajectory Example: Simple Action Client
-
-    Creates a client of the Joint Trajectory Action Server
-    to send commands of standard action type,
-    control_msgs/FollowJointTrajectoryAction.
-
-    Make sure to start the joint_trajectory_action_server.py
-    first. Then run this example on a specified limb to
-    command a short series of trajectory points for the arm
-    to follow.
-    """
-    # arg_fmt = argparse.RawDescriptionHelpFormatter
-    # parser = argparse.ArgumentParser(formatter_class=arg_fmt,
-    #                                  description=main.__doc__)
-    # required = parser.add_argument_group('required arguments')
-    # required.add_argument(
-    #     '-l', '--limb', required=True, choices=['left', 'right'],
-    #     help='send joint trajectory to which limb'
-    # )
-    # args = parser.parse_args(rospy.myargv()[1:])
-    limb = 'right'
 
     print("Initializing node... ")
     rospy.init_node("throwing")
-    print("Getting robot state... ")
-    rs = baxter_interface.RobotEnable(CHECK_VERSION)
-    print("Enabling robot... ")
-    rs.enable()
-    print("Running. Ctrl-c to quit")
-    positions = {
-        'left':  [-0.11, -0.62, -1.15, 1.32,  0.80, 1.27,  2.39],
-        'right':  [0.11, -0.62,  1.15, 1.32, -0.80, 1.27, -2.39],
-    }
 
- #    q_start = np.array([0.2339320701525256,  -0.5878981369570848,  0.19903400722813244,  1.8561167533413507,
- # -0.4908738521233324,  -0.97752925707998,  -0.49547579448698864])
- #    q_throw = np.array([0.9265243958827899,  -0.7827136970185323,  -0.095490304045867,  1.8338740319170121,
- #     -0.03681553890924993,  -0.9909515889739773,  -0.5840631849873713])
- #    q_end = np.array([0.9085001216251363,  -1.0089758632316308, 0.07401457301547121, 1.8768254939778037,
- #     0.18599517053110642, -0.8172282647459542, -0.44600491407768406])
-
-
-    q_throw = np.array([ 0.47668453, -0.77274282,  0.93150983,  2.08352941,  0.54149522,
-       -1.26745163, -2.06742261])
-    q_end = np.array([ 0.75356806, -0.89162633,  0.54648066,  2.08698086,  0.41033986,
-       -1.18423317, -1.15815549])
-    q_start = np.array([-0.22281071, -0.36470393,  0.36163597,  1.71920897, -0.82719914,
-       -1.16889336, -0.90888362])
-
-    traj = Trajectory(limb)
+    traj = Trajectory('right')
+    traj._t_delay = 5.0
     rospy.on_shutdown(traj.stop)
     # Command Current Joint Positions first
-    limb_interface = baxter_interface.limb.Limb('right')
-
-    rospy.sleep(.5)
-    current_angles = [limb_interface.joint_angle(joint) for joint in limb_interface.joint_names()]
-    traj.add_point_p(current_angles, 0.0)
-    traj._t_delay = 5.0
-
-    # T = 1.7
-    # N = 50*T
-    # dt = float(T)/(N-1)
-    # vy = .8 # nominal .8
-    # vz = .4 # nominal .4
-    # jerk = -20 #nominal -5
-
-    # # plt.close('all')
-
-    # thList, vList = linearSpace(False, T, N, vy, vz, jerk)
-    # # thList = thList[1:,:]
-    # # vList = np.diff(thList,axis=0)/dt
-    # # vList = np.vstack((np.array([0,0,0,0,0,0,0]),vList))
-    # aList = np.diff(vList,axis=0)/dt
-    # aList = np.vstack((np.array([0,0,0,0,0,0,0]),aList))
-    # t_all = np.linspace(0 + t_delay,2*T + t_delay, N*2);
-
-    # plt.figure()
-    # plt.plot(thList)
-    # plt.title('Position (joint)')
-    # plt.show(block=False)
-    # plt.figure()
-    # plt.title('Velocity (joint)')
-    # plt.plot(vList)
-    # plt.show(block=False)
-    # plt.figure()
-    # plt.title('Acceleration (joint)')
-    # plt.plot(aList)
-    # plt.show(block=False)
-
-    traj.add_point_p(thList[0,:].tolist(),t_delay)
-
-    # for i in range(int(2*N)-2):
-    #     traj.add_point(thList[i,:].tolist(), vList[i,:].tolist(), aList[i,:].tolist(),t_all[i])
-
-    # traj.start()
-
-    # traj.wait(30.0)
-    # traj.clear(limb)
 
     rospy.spin()
 
